5f00e19789dcfe147d7eeae72042bfb55892e7a4
Rosso84 <roosbeh@hotmail.com>
2018-09-03 11:30:51 +0200

    added basic testcases

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,19 @@
-public class App     {
+public class App {
+
+
+    int i = 2;
+
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("hello wøøørld");
+    }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,2 +1,24 @@
+
+
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+
 public class AppTest {
+
+
+    @Test
+    public void getI() {
+        int i = 2;
+
+        App foo = new App();
+
+        int iVariable = foo.getI();
+
+        assertEquals(i, iVariable);
+    }
+
 }
